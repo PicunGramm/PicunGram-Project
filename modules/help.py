0ea7e9c057f45defcf90bebdb2a60daf911b34d4
PicunGram <tiger0gd@outlook.com>
2025-05-12 22:15:35 +0300

    help

---
+++ modules/help.py
@@ -0,0 +1,15 @@
+from telethon import TelegramClient, events
+
+async def setup(client):
+    @client.on(events.NewMessage(pattern=r'\.help'))
+    async def handler(event):
+        if event.sender_id != (await client.get_me()).id:
+            return
+        try:
+            await event.delete()
+        except Exception as e:
+            print(f"Error {e}")
+        
+        msg = await event.reply("**Помощь:**\n__В начале везде точка__\n**spam** `<кол-во> <текст>`\n**doks**\n**секс**`(в ответ на сообщение)`\n**ping**\n**help**`(вызывает это меню)`")
+
+    print("Модуль запущен!(.help)")
