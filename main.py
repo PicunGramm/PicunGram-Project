b156cb8e51f339839f6a0fd353bec9bbd2145bc2
PicunGram <tiger0gd@outlook.com>
2025-06-04 23:43:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from telethon.sync import TelegramClient
 import asyncio
 from importlib import import_module
 import os
+import time 
 
 api_id = 26881961
 api_hash = 'f3e8b6c96cbbb02eca435c60318ff338'
 session_name = 'session_name'
 modules_dir = 'modules'
 number_file = 'number.txt'
+
+def clear_terminal():
+    os.system('clear')
+
+clear_terminal()
 
 def get_phone_numder():
     """Получает номер телефона: из файла или запрашивает у пользователя"""
@@ -19,22 +25,29 @@ def get_phone_numder():
     
     while True:
         try:
-            phone = input("Введите номер телефона (в формате +79123456789): ").strip()
+            print("\033[32m██████╗ ██╗ ██████╗██╗   ██╗███╗   ██╗ ██████╗ ██████╗  █████╗ ███╗   ███╗\n██╔══██╗██║██╔════╝██║   ██║████╗  ██║██╔════╝ ██╔══██╗██╔══██╗████╗ ████║\n██████╔╝██║██║     ██║   ██║██╔██╗ ██║██║  ███╗██████╔╝███████║██╔████╔██║\n██╔═══╝ ██║██║     ██║   ██║██║╚██╗██║██║   ██║██╔══██╗██╔══██║██║╚██╔╝██║\n██║     ██║╚██████╗╚██████╔╝██║ ╚████║╚██████╔╝██║  ██║██║  ██║██║ ╚═╝ ██║\n╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝\033[0m")
+            phone = input("\033[33m╔══════════════════════════════════════════════════─\n║\n╚\033[0m \033[35mВведите номер телефона (в формате +79123456789):\033[0m ").strip()
             if not phone.startswith('+'):
-                print("Номер должен начинаться с '+'")
+                print("\033[35mНомер должен начинаться с '+'\033[0m")
+                time.sleep(5)
+                clear_terminal()
                 continue
             if not phone[1:].isdigit():
-                print("Номер должен содержать только цифры после '+'")
+                print("\033[35mНомер должен содержать только цифры после '+'\033[0m")
+                time.sleep(5)
+                clear_terminal()
                 continue
             if len(phone) < 12:
-                print("Слишком короткий номер")
+                print("\033[35mСлишком короткий номер\033[0m")
+                time.sleep(5)
+                clear_terminal()
                 continue
 
             with open(number_file, 'w') as f:
                 f.write(phone)
             return phone
         except Exception as e:
-            print(f"ОШИБКА: {e}. Попробуйте снова")
+            print(f"\033[35mОШИБКА: {e}. Попробуйте снова\033[0m")
 
 async def load_module(client, module_name):
     """Загружает и инициализирует модуль"""
@@ -43,19 +56,19 @@ async def load_module(client, module_name):
         if hasattr(module, 'setup'):
             await module.setup(client)
         else:
-            print(f"В {module_name} нет функции setup()")
+            print(f"\033[35mВ {module_name} нет функции setup()\033[0m")
     except Exception as e:
-        print(f"ОШИБКА!!! В {module_name}: {str(e)}")
+        print(f"\033[35mОШИБКА!!! В {module_name}: {str(e)}\033[0m")
 
 async def main():
     phone = get_phone_numder()
-    print(f"Используется номер: {phone}")
+    print(f"\033[35mИспользуется номер: {phone}\033[0m")
     
     client = TelegramClient('session', api_id, api_hash)
 
     try:
         await client.start(phone)
-        print("Авторизация прошла успешно!!!")
+        print("\033[35mАвторизация прошла успешно!!!\033[0m")
 
         module_files = [
             f[:-3] for f in os.listdir(modules_dir)
@@ -65,11 +78,11 @@ async def main():
         for module_name in module_files:
             await load_module(client, module_name)
 
-        print("Бот запущен! Ожидание команд...")
+        print("\033[35mУспешно!\033[0m")
         await client.run_until_disconnected()
 
     except Exception as e:
-        print(f"ОШИБКА!!! : {str(e)}")
+        print(f"\033[35mОШИБКА!!! : {str(e)}\033[0m")
     finally:
         await client.disconnect()
 
